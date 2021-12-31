96e9db3658de4f37e574ae1c8d03928132bca496
Víctor O. Vento <victorovento@icloud.com>
2021-12-31 16:04:01 -0500

    All ready

--- controllers/user.js
+++ controllers/user.js
@@ -1,39 +1,55 @@
-const { response } = require("express");
+const {
+    response
+} = require("express");
 
-const getUsers = (req, res) => {
-  res.status(501).json({
-    msg: "GET Endpoint",
-  });
+const getUsers = (req, res = response) => {
+
+    const query = req.query;
+    res.status(501).json({
+        msg: "GET Endpoint",
+        query
+    });
 };
 
-const postUsers = (req, res) => {
-  res.status(501).json({
-    msg: "POST Endpoint",
-  });
+const postUsers = (req, res = response) => {
+    const {
+        nombre,
+        edad
+    } = req.body;
+
+    res.status(501).json({
+        msg: "POST Endpoint",
+        nombre,
+        edad,
+    });
 };
 
-const patchUsers = (req, res) => {
-  res.status(501).json({
-    msg: "PATCH Endpoint",
-  });
+const patchUsers = (req, res = response) => {
+    res.status(501).json({
+        msg: "PATCH Endpoint",
+    });
 };
 
-const putUsers = (req, res) => {
-  res.status(501).json({
-    msg: "PUT Endpoint",
-  });
+const putUsers = (req, res = response) => {
+    const {
+        id
+    } = req.params;
+    res.status(501).json({
+        msg: "PUT Endpoint",
+        id
+    });
 };
 
-const deleteUsers = (req, res) => {
-  res.status(501).json({
-    msg: "DELETE Endpoint",
-  });
+const deleteUsers = (req, res = response) => {
+    res.status(501).json({
+        msg: "DELETE Endpoint",
+    });
 };
 
 module.exports = {
-  getUsers,
-  patchUsers,
-  putUsers,
-  deleteUsers,
-  postUsers,
+    getUsers,
+    patchUsers,
+    putUsers,
+    deleteUsers,
+    postUsers,
 };
--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ const router = Router();
 
 router.get("/", getUsers);
 
-router.put("/", putUsers);
+router.put("/:id", putUsers);
 
 router.post("/", postUsers);
 
